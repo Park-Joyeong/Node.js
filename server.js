fb826b32760f218f7c9797f7a7f829a84806960a
Park_Joyeong <drhamst@gmail.com>
2021-06-28 17:21:36 +0900

    commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require("http").Server(app);
 var io = require('socket.io')(http);
 
 var seq = 0;
-var userList = [];
+var clients = {};
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -13,12 +13,56 @@ app.get("/", function (req, res) {
 
 io.on('connection', function (socket) {
   seq = seq + 1;
-  userId = 'user_' + seq
-  userList.concat(userId)
-  console.log('new connection')
-  socket.emit('new', '{"userId":' + userId + '}');
+  userId = 'User_' + seq
+  clients[socket.id] = userId;
+  var sendObj = {}
+  sendObj['userId'] = userId;
+  sendObj['socketId'] = socket.id;
+  sendObj['clients'] = clients;
+  socket.emit('forMe', sendObj);
+  io.emit('new', sendObj);
+
+  socket.on('sendMessage', function (param) {
+
+    console.log(socket);
+    var senderSocketID = socket.id;
+    var targetSocketID = param.socketID;
+    var message = param.message;
+
+    sendObj = {};
+    sendObj['senderSocketID'] = senderSocketID;
+    sendObj['targetSocketID'] = targetSocketID;
+    sendObj['message'] = message;
+
+    if (targetSocketID === '') {
+      console.log('For everyone');
+      io.emit('receiveMessage', sendObj);
+    } else {
+      console.log('For one persons');
+    }
+    // console.log(targetSocketID);
+    // console.log(message);
+
+    //   sendObj['userId'] = userId;
+    // sendObj['clients'] = clients;
+    // socket.emit('forMe', sendObj);
+    // io.emit('new', sendObj);
+
+    //io.emit('receiveMessage', )
+    // seq = seq + 1;
+    // userId = 'User_' + seq
+    // clients[socket.id] = userId;
+    // var sendObj = {}
+    // sendObj['userId'] = userId;
+    // sendObj['clients'] = clients;
+    // socket.emit('forMe', sendObj);
+    // io.emit('new', sendObj);
+
+  });
 
 });
+
+
 
 http.listen(8000, function () {
   console.log("Listening on *:8000");
