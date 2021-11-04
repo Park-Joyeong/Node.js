b5891d10bdc3c326546eabc87c0767ac9ca0a73a
Park Joyeong <drhamst@gmail.com>
2021-11-04 22:57:58 +0900

    Studying chap 07

---
+++ oreilly/ch07/meadowlark.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
